5af1e38ce5d5d104242800643f8c33d7c93bea06
Kelly Barkhurst <kellybarkhurst@gmail.com>
2021-05-05 19:39:26 -0400

    Working on seeding joys

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 User.create!(username: "Patricia", first_name: "Patricia", last_name: "Barnes", journal_description:'Patricia, the site’s namesake, battled breast cancer for almost 9 years during which she developed “awe-inspiring” coping skills and, by her example, taught those around her to live each day to the fullest. She found purpose and took comfort in actively acknowledging life’s simple, daily pleasures, as she dealt with the challenges presented by her disease. This “record of joy” was an intimate way for her family to view her life, separate from her diesease, and to view the family’s lives through her eyes. Journal content was woven into Christmas letters, health updates and e-mails. Patricia talked about her journal writing at support groups and functions. She inspired others to keep records of their own joys, and her story reached many in the breast-cancer community. Her first journal entry reads, “This is a very special journal because it is a record of the many blessings the Lord has generously given me at all times and in all circumstances. Anyone has my permission to read this journal. It is an account of joy!”', email: "patricia@example.com", password: "password")
 User.create!(username: "Kelly", first_name: "Kelly", last_name: "Barkhurst", journal_description:"Daughter of Patricia Barnes, the site's inspiration, I'm a mother, designer, wife and friend trying to live each day to the fullest.", email: "kelly@example.com", password: "password")
 require "csv"
-csv_text = File.read(Rails.root.join("lib", "seeds", "patricias_joys.csv"))
+csv_text = File.read(Rails.root.join("lib", "seeds", "patricia_joy_journey.csv"))
 csv = CSV.parse(csv_text, :headers => true, :encoding => "utf-8")
 csv.each do |row|
   t = Joy.new
@@ -20,7 +20,18 @@ csv.each do |row|
   t.save
   puts "#{t.body}, #{t.created_at} saved"
 end
-
+csv_text = File.read(Rails.root.join("lib", "seeds", "patricia_joy_journey2.csv"))
+csv = CSV.parse(csv_text, :headers => true, :encoding => "utf-8")
+csv.each do |row|
+  t = Joy.new
+  t.body = row["body"]
+  t.user_id = 1
+  t.visibility = true
+  t.created_at = Date.strptime(row[0], "%e-%b-%y")
+  t.updated_at = Date.strptime(row[0], "%e-%b-%y")
+  t.save
+  puts "#{t.body}, #{t.created_at} saved"
+end
 
 
 
